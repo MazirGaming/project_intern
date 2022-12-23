cb9cd9a32a39b248dd89c3fde52d15fc0d88bda8
MazirGaming <nguyenmanh10a7@gmail.com>
2022-12-23 11:25:49 +0700

    remove comment

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -41,13 +41,6 @@
                                                 <td>{{$user->phone}}</td>
                                                 <td>{{$user->email}}</td>
                                                 <td>{{$user->role_name}}</td>
-                                                <!-- @if ($user->type == 1)
-                                                <td>Admin</td>
-                                                @elseif ($user->type == 3)
-                                                <td>Student</td>
-                                                @else
-                                                <td></td>
-                                                @endif -->
                                                 <td class="text-end">
                                                 <div class="actions">
                                                     <a href="{{route('user.edit', ['user' => $user->id])}}" class="btn btn-sm bg-success-light me-2">
