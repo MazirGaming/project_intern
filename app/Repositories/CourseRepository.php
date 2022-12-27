40f896231f80f423cd47c69a3ef07bbc73d3bc9c
MazirGaming <nguyenmanh10a7@gmail.com>
2022-12-27 12:47:14 +0700

    fix

--- app/Repositories/CourseRepository.php
+++ app/Repositories/CourseRepository.php
@@ -13,9 +13,6 @@ class CourseRepository extends BaseRepository
 
     public function getByUserId($input)
     {
-        return $this->model->whereRelation('CourseUser', 'user_id', $input, function ($query) {
-            $query->whereRelation('Category', 'categories.id', 'courses.category_id')
-                ->select('courses.*', 'categories.name as category');
-        })->get();
+        return $this->model->whereRelation('Users', 'users.id', $input)->get();
     }
 }
